5b1984dd07513acd49dec37522f6da5ea8947080
jinit-chaudhari <24bit132@sot.pdpu.ac.in>
2025-03-19 23:30:47 +0530

    Add files via upload
    Python Lab-2

---
+++ lab2-2.py
@@ -0,0 +1,9 @@
+a = int(input("enter a number :"))
+b = int(input("enter a number :"))
+c = int(input("enter a number :"))
+if(b>a and b>c):
+    print("b is greatest")
+elif(c>a and c>b):
+    print("c is greatest")
+else:
+    print("a is greatest")
